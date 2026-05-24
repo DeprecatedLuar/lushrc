3a3f663d82348e7cd702b2838063e55ee9216491
DeprecatedLuar <rauassis2@gmail.com>
2026-05-24 14:58:50 -0300

    yolo commit

--- bin/lib/vibecheck/find-process.sh
+++ bin/lib/vibecheck/find-process.sh
@@ -27,7 +27,7 @@ else
         exit 1
     fi
 
-    pids=$(pgrep -i "$name" 2>/dev/null || pgrep "$name")
+    pids=$(pgrep -if "$name" 2>/dev/null || pgrep -f "$name")
 
     if [[ -z "$pids" ]]; then
         pids=$(ps -eo pid,comm --no-headers | fzf --filter="$name" -1 | awk '{print $1}')
