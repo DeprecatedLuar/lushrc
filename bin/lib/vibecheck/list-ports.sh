5ecd902ecb014e2b51cc1d9a59f5130247f506e3
DeprecatedLuar <rauassis2@gmail.com>
2026-06-03 09:43:58 -0300

    yolo commit

--- bin/lib/vibecheck/list-ports.sh
+++ bin/lib/vibecheck/list-ports.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
+# Handle --all/-a flag for showing system services
+if [[ "$1" == "--all" || "$1" == "-a" ]]; then
+    if [[ $EUID -ne 0 ]]; then
+        # Not root, re-execute with sudo
+        exec sudo "$0"
+    fi
+fi
+
 lsof -i -P -n | awk 'NR>1 && /LISTEN/ {
     split($9, a, ":")
     port = a[length(a)]
